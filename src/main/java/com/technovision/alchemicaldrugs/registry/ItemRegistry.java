318ecf9747dc586141203dd36bbc0a6b9219a67d
TechnoVisionDev <thomasanthonypeters@gmail.com>
2022-08-07 02:05:05 -0700

    Add penicillin, update license, and update icon

--- src/main/java/com/technovision/alchemicaldrugs/registry/ItemRegistry.java
+++ src/main/java/com/technovision/alchemicaldrugs/registry/ItemRegistry.java
@@ -18,6 +18,7 @@ public class ItemRegistry {
     public static PsilocybinItem PSILOCYBIN = new PsilocybinItem();
     public static ItemWithTooltip HYDROCODONE = new ItemWithTooltip("C₁₈H₂₁NO₃");
     public static ItemWithTooltip DINITROGEN_TETROXIDE = new ItemWithTooltip("N₂O₄");
+    public static ItemWithTooltip PENICILLIN = new ItemWithTooltip("C₁₆H₁₈(N₂O₄)S");
     public static AcidTabItem ACID_TAB = new AcidTabItem();
     public static Item SYRINGE = new Item(ITEM_SETTINGS);
     public static AdrenalineItem ADRENALINE_SHOT = new AdrenalineItem();
@@ -36,6 +37,7 @@ public class ItemRegistry {
         Registry.register(Registry.ITEM, new Identifier(AlchemicalDrugs.MOD_ID, "psilocybin"), PSILOCYBIN);
         Registry.register(Registry.ITEM, new Identifier(AlchemicalDrugs.MOD_ID, "hydrocodone"), HYDROCODONE);
         Registry.register(Registry.ITEM, new Identifier(AlchemicalDrugs.MOD_ID, "dinitrogen_tetroxide"), DINITROGEN_TETROXIDE);
+        Registry.register(Registry.ITEM, new Identifier(AlchemicalDrugs.MOD_ID, "penicillin"), PENICILLIN);
         Registry.register(Registry.ITEM, new Identifier(AlchemicalDrugs.MOD_ID, "acid_tab"), ACID_TAB);
         Registry.register(Registry.ITEM, new Identifier(AlchemicalDrugs.MOD_ID, "syringe"), SYRINGE);
         Registry.register(Registry.ITEM, new Identifier(AlchemicalDrugs.MOD_ID, "adrenaline_shot"), ADRENALINE_SHOT);
